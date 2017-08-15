c319cd0f931b0b666b0615af9c175d0a32722d8f
VinLau <vinlau@users.noreply.github.com>
2017-08-15 18:24:25 -0400

    halfway through play game

--- gmatgamefrontend/src/pages/PlayGame.js
+++ gmatgamefrontend/src/pages/PlayGame.js
@@ -18,42 +18,50 @@ export default class PlayGame extends Component {
       .then( res => {
         this.setState({
           Q : res.body.title,
-          A1: res.body.answerA,
-          A2: res.body.answerB,
-          A3: res.body.answerC,
-          A4: res.body.answerD,
-          A5: res.body.answerE,
+          A: res.body.answerA,
+          B: res.body.answerB,
+          C: res.body.answerC,
+          D: res.body.answerD,
+          E: res.body.answerE,
           correctAns: res.body.correctAnswer,
           cat : res.body.category,
           level : res.body.level
         })
       })
-      .catch(console.log)
+      .catch( error => {
+        this.setState({
+          Q: "Error loading Question: " + error
+        })
+      })
   }
 
   componentDidMount(){
-    this.fetchQAndA(200, true);
+    this.fetchQAndA(); //By default if we don't send the level param and the correct boolean and will start at the initial question
   }
 
-  componentDidUpdate(prevProps, prevState){
-    this.fetchQAndA();
+  calculateCorrectAns = (answer) => {
+    if (this.state.correctAns === answer){
+      console.log("CORRECT!");
+    }
+  }
+
+  componentDidUpdate(){
+    
   }
 
   render(){
     return(
       <div>
         <div className="description-container">
-          <DescriptiveTextBox bgColor="olive" color="white" theText={"SCORE:" + this.state.score}/>
-          <DescriptiveTextBox bgColor="babyblue" color="white" theText={this.state.cat}/>
-          <DescriptiveTextBox bgColor="lightred" color="white" theText={this.state.level}/>
+          <DescriptiveTextBox bgColor="olive" color="white" theText={"SCORE: " + this.state.score}/>
+          <DescriptiveTextBox bgColor="blue" color="white" theText={"Category: " + this.state.cat}/>
+          <DescriptiveTextBox bgColor="red" color="white" theText={this.state.level}/>
         </div>
         <div className="game-container">
           <Question questionText={this.state.Q}/>
-          <Answer answerText={this.state.A1}/>
-          <Answer answerText={this.state.A2}/>
-          <Answer answerText={this.state.A3}/>
-          <Answer answerText={this.state.A4}/>
-          <Answer answerText={this.state.A5}/>
+          {
+            ['A','B','C','D','E'].map(ans => <Answer answerText={this.state[ans]} onClick={() => this.calculateCorrectAns(ans)} />)
+          }
         </div>
       </div>
     )
