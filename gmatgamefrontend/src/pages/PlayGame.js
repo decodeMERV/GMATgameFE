3fcce7d1b861822a2dd8c791359f9cdab009e914
RachelElhamoui <elhamoui.rachel@gmail.com>
2017-08-18 18:58:07 -0400

    added time value in api.js and PlayGame.js

--- gmatgamefrontend/src/pages/PlayGame.js
+++ gmatgamefrontend/src/pages/PlayGame.js
@@ -28,7 +28,7 @@ export default class PlayGame extends Component {
           D: res.body.answerD,
           E: res.body.answerE,
           correctAns: res.body.correctAnswer,
-          cat : res.body.catcategoryId,
+          cat : res.body.categoryId,
           cat_name : res.body.categoryName,
           level : res.body.level,
           isPlayerCorrect : undefined,
@@ -88,17 +88,18 @@ export default class PlayGame extends Component {
     var questionId = this.state.ID;
     var category = this.state.cat;
     var level = this.state.level;
+    var time = this.state.timeElapsed;
 
 
 
-    console.log("pts ", score)
+    console.log("TIME ", time)
 
     //api.recordQuestion(this.props.username, this.state.ID, rw, this.state.cat, answer, this.state.level, pts)
-      api.recordQuestion(username,questionId,isCorrect,category,answer,level,score)
+      api.recordQuestion(username,questionId,isCorrect,category,answer,level,score,time)
       .then(res => {console.log("res",res)})
       .catch(() => this.setState({theError: "Wrong database command"}))
-    console.log("all this chit: ", this.props.username, this.state.ID, this.state.cat, answer, this.state.level)
-    console.log("all this cheet", username,questionId,isCorrect,category,answer,level,score);
+    console.log("all this chit: ", this.props.username, this.state.ID, this.state.cat, answer, this.state.level,this.state.timeElapsed)
+    console.log("all this cheet", username,questionId,isCorrect,category,answer,level,score,time);
   }
 
   colorChoices = (answersAtoE) => {
