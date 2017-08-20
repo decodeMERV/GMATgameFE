b07c51783c43caed8b6fc443bd215eb103ec35d2
RachelElhamoui <elhamoui.rachel@gmail.com>
2017-08-20 01:26:54 -0400

    implemented update profilebox on ProfileBox,js, UserProfile.js

--- gmatgamefrontend/src/elements/ProfileBox.js
+++ gmatgamefrontend/src/elements/ProfileBox.js
@@ -2,14 +2,18 @@ import React from 'react';
 import './ProfileBox.css';
 
 export default (props) => {
+  console.log(props.button, 'inside profile box')
   return (
     <div className="profilebox">
       <h2>Username</h2>
-      <h3>{props.name}</h3>
+      {props.editOpen === true && props.button === 'name' ? <input defaultValue={props.name}/> : <h3>{props.name}</h3>}
+      <button name='name' onClick={(e) => props.toggle(e)}>Edit</button>
       <h2>E-Mail</h2>
-      <h4>{props.contact}</h4>
+      {props.editOpen === true && props.button === 'email' ? <input placeholder={props.contact}/> : <h3>{props.contact}</h3>}
+      <button name='email' onClick={(e) => props.toggle(e)}>Edit</button>
       <h2>Interests</h2>
-      <h4>{props.aboutMe}</h4>
+      {props.editOpen === true && props.button === 'aboutMe' ? <input placeholder={props.aboutMe}/> : <h3>{props.aboutMe}</h3>}
+      <button name="aboutMe" onClick={(e) => props.toggle(e)}>Edit</button>
     </div>
   )
 }
