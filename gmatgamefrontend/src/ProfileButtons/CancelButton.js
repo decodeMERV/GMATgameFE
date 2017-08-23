412fa24bcb9cb9c81b072f4d3fe1a241584d30ee
RachelElhamoui <elhamoui.rachel@gmail.com>
2017-08-23 02:17:19 -0400

    css

--- gmatgamefrontend/src/ProfileButtons/CancelButton.js
+++ gmatgamefrontend/src/ProfileButtons/CancelButton.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
 export default (props) => (
-  <div className="cancel-button">
     <i className="fa fa-times fa-2x" onClick={this.onClick}></i>
-  </div>
 )
--- gmatgamefrontend/src/ProfileButtons/EditButton.js
+++ gmatgamefrontend/src/ProfileButtons/EditButton.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
 export default (props) => (
-  <div className="edit-button">
     <i className="fa fa-pencil-square-o fa-2x" onClick={this.onClick}/>
-  </div>
 );
